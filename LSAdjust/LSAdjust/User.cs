bc57dcd3de7a9b43e05f6d68413bb12a82dcd65c
YU Zhouwei <632602112@qq.com>
2017-12-08 22:10:20 +0800

    init

---
+++ LSAdjust/LSAdjust/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace LSAdjust
+{
+    //连接服务器玩家信息
+    class User
+    {
+        public TcpClient client;
+        public StreamReader sr;
+        public StreamWriter sw;
+        public string userName;
+
+        public bool isPlaying=false;
+
+        public User(TcpClient client)
+        {
+            this.client = client;
+            this.userName = "";
+            NetworkStream ns = client.GetStream();
+            sr = new StreamReader(ns, System.Text.Encoding.UTF8);
+            sw = new StreamWriter(ns, System.Text.Encoding.UTF8);
+        }
+    }
+}
